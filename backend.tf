09b3b41d04f2af4fd1a3764279d1980a27303d9d
Imtronick <Cloudsolutionlatam@outlook.com>
2025-12-22 18:06:36 -0500

    Update storage account name to unique value

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstateapislabs"
+    storage_account_name = "tfstateapislabsdev"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
