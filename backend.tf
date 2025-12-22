dcf59ead33de3c92c4aaa498e26b255196b6127d
Imtronick <Cloudsolutionlatam@outlook.com>
2025-12-22 18:11:57 -0500

    Fix backend storage with fixed unique name

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstateapislabsdev"
+    storage_account_name = "tfstateimtronick2025"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
