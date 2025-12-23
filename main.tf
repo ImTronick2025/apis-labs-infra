48273de952f63c3e9940f5aba65dcb8ddb813931
Imtronick <Cloudsolutionlatam@outlook.com>
2025-12-22 20:10:33 -0500

    Fix flex runtime version format

--- main.tf
+++ main.tf
@@ -183,7 +183,7 @@ resource "azurerm_function_app_flex_consumption" "main" {
   storage_authentication_type = "StorageAccountConnectionString"
   storage_access_key          = azurerm_storage_account.functions.primary_access_key
   runtime_name                = "dotnet-isolated"
-  runtime_version             = "8"
+  runtime_version             = "8.0"
   maximum_instance_count      = 50
   instance_memory_in_mb       = 2048
 
