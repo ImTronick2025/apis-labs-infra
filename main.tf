d4a8b46350e7486314c2532be0274d90de17b505
Imtronick <Cloudsolutionlatam@outlook.com>
2025-12-23 09:09:35 -0500

    Add Azure Key Vault to infra

--- main.tf
+++ main.tf
@@ -26,9 +26,16 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+data "azurerm_client_config" "current" {}
+
 locals {
   functions_storage_account_name = substr(
     lower(replace("${var.prefix}func${random_string.suffix.result}", "-", "")),
+    0,
+    24
+  )
+  key_vault_name = substr(
+    lower(replace("${var.prefix}kv${random_string.suffix.result}", "-", "")),
     0,
     24
   )
@@ -139,6 +146,46 @@ resource "azurerm_application_insights" "main" {
   lifecycle {
     ignore_changes = [workspace_id]
   }
+}
+
+# Key Vault for secrets
+resource "azurerm_key_vault" "main" {
+  name                = local.key_vault_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled  = false
+  enable_rbac_authorization = false
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+      "List",
+      "Create",
+      "Delete",
+      "Backup",
+      "Restore",
+      "Recover"
+    ]
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Backup",
+      "Restore",
+      "Recover",
+      "Purge"
+    ]
+  }
+
+  tags = var.tags
 }
 
 # Storage Account para Azure Functions (requerido por Flex Consumption)
--- outputs.tf
+++ outputs.tf
@@ -71,3 +71,13 @@ output "functions_storage_account_name" {
   description = "Nombre del Storage Account de Functions"
   value       = azurerm_storage_account.functions.name
 }
+
+output "key_vault_name" {
+  description = "Nombre del Key Vault"
+  value       = azurerm_key_vault.main.name
+}
+
+output "key_vault_uri" {
+  description = "URI del Key Vault"
+  value       = azurerm_key_vault.main.vault_uri
+}
